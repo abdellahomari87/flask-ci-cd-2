fad89856a931b37b49ebf679c6c066b2299276d5
abdellahomari87 <abdellah.omari87@gmail.com>
2025-08-15 17:33:59 +0200

    fix: add browser polyfills + webpack fallbacks

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,25 @@
-const { override, addWebpackResolve } = require('customize-cra');
+// config-overrides.js
+const webpack = require('webpack');
 
-module.exports = override(
-  addWebpackResolve({
-    fallback: {
-      http: require.resolve('stream-http'),
-    },
-  }),
-);
+module.exports = function override(config) {
+  config.resolve = config.resolve || {};
+  config.resolve.fallback = {
+    ...(config.resolve.fallback || {}),
+    path: require.resolve('path-browserify'),
+    fs: false,
+    os: false,
+    crypto: false,
+    http: false,
+    https: false,
+    stream: false,
+  };
+
+  config.plugins = (config.plugins || []).concat([
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+      Buffer: ['buffer', 'Buffer'],
+    }),
+  ]);
+
+  return config;
+};
